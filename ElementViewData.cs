55dfb34c82b19d3813603ab8e93f90d9356adf35
igor.semenov <igor.semenov.minsk@gmail.com>
2023-04-09 21:20:33 +0200

    Refactoring

---
+++ ElementViewData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public readonly struct ElementViewData
+{
+    public readonly Vector2 Min;
+    public readonly Vector2 Max;
+    public readonly Vector2 Position;
+
+    public ElementViewData(Vector2 position, Vector2 size)
+    {
+        Position = position;
+        Min = position - size * 0.5f;
+        Max = position + size * 0.5f;
+    }
+}
