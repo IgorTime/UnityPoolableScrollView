b979cdd378aa33779953d967c46248119c637009
igor.semenov <igor.semenov.minsk@gmail.com>
2023-04-23 20:29:26 +0200

    Refactoring

--- HorizontalScroll.cs
+++ HorizontalScroll.cs
@@ -4,6 +4,11 @@ using UnityEngine.UI;
 [RequireComponent(typeof(ScrollRect))]
 public class HorizontalScroll : PoolableScroll
 {
+    public override void ScrollToItem(int itemIndex)
+    {
+        Content.anchoredPosition = new Vector2(-ViewsData[itemIndex].Position.x - ViewportWidth * 0.5f, 0);
+    }
+    
     protected override bool IsMovingForward(in Vector2 contentDeltaPosition) => contentDeltaPosition.x < 0;
 
     protected override bool IsFastScrolling(in Vector2 deltaPosition) =>
@@ -26,18 +31,18 @@ public class HorizontalScroll : PoolableScroll
         contentSize = new Vector2(contentWidth, Content.sizeDelta.y);
     }
 
-    protected override bool IsOutOfViewportInForwardDirection(int itemIndex, in Vector2 anchoredPosition) =>
-        ViewsData[itemIndex].Min.x > -anchoredPosition.x + ViewportWidth; // IsOnTheRightOfViewport
+    protected override bool IsOutOfViewportInForwardDirection(int itemIndex, in Vector2 contentAnchoredPosition) =>
+        ViewsData[itemIndex].Min.x > -contentAnchoredPosition.x + ViewportWidth; // IsOnTheRightOfViewport
 
-    protected override bool IsOutOfViewportInBackwardDirection(int itemIndex, in Vector2 anchoredPosition) =>
-        ViewsData[itemIndex].Max.x < -anchoredPosition.x; // IsOnTheLeftOfViewport
+    protected override bool IsOutOfViewportInBackwardDirection(int itemIndex, in Vector2 contentAnchoredPosition) =>
+        ViewsData[itemIndex].Max.x < -contentAnchoredPosition.x; // IsOnTheLeftOfViewport
 
     protected override Vector2 CalculateItemPositionInContent(in int itemIndex) =>
         new(-ContentRect.width * 0.5f + ViewsData[itemIndex].Position.x, 0);
 
-    protected override bool IsPartiallyVisibleInViewport(in int elementIndex, in Vector2 anchoredPosition) =>
-        !IsOutOfViewportInBackwardDirection(elementIndex, anchoredPosition) &&
-        !IsOutOfViewportInForwardDirection(elementIndex, anchoredPosition);
+    protected override bool IsPartiallyVisibleInViewport(in int elementIndex, in Vector2 contentAnchoredPosition) =>
+        !IsOutOfViewportInBackwardDirection(elementIndex, contentAnchoredPosition) &&
+        !IsOutOfViewportInForwardDirection(elementIndex, contentAnchoredPosition);
 
     protected override int FindFirstItemVisibleInViewport(in Vector2 contentAnchoredPosition)
     {
@@ -66,10 +71,5 @@ public class HorizontalScroll : PoolableScroll
                 startIndex = middleIndex + 1;
             }
         }
-    }
-
-    public override void ScrollToItem(int itemIndex)
-    {
-        
     }
 }
